877e4d96c8c0d26d86161f9cd0b1336c36f6c543
Michael Jones <michaeljones@Michaels-Mac-mini.local>
2014-04-27 18:32:33 +0100

    fixing the single Announcements post page

---
+++ Starkers/single-announcement.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers 3.0
+ */
+
+get_header(); ?>
+<div class="container">
+<div class="row">
+
+<?php get_sidebar(); ?>
+<!--
+<h3>Popular artists available on CD</h3><?php wp_tag_cloud( array ( 'taxonomy' => artist ) ); ?>
+<h3>Popular subgenres</h3><?php wp_tag_cloud( array ( 'taxonomy' => category ) ); ?>
+-->
+
+<div class="span9">
+<div class="sub-hero-unit">
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); 
+$imagepath = strip_tags( get_the_term_list($post->ID, 'imagepath', '', ', ', '')); 
+if($imagepath!=''){
+echo "<img style='float:left;margin:10px;margin-top:0;width:250px;' src='" . $imagepath . "'/>"; }
+?>
+<?php the_post_thumbnail(); ?>
+<h1><?php echo "<b>".get_the_time('F j, Y')."</b>"; ?></h1>
+<?php 
+if($content = $post->post_content ) {
+the_content();
+}
+?>
+</div><!--sub-hero-unit -->
+
+</div><!-- row-fluid -->
+</div><!-- container-fluid -->
+<?php endwhile; // end of the loop. ?>
+
+
+<?php get_footer(); ?>
